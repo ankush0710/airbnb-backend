da02149d237027740628368c903fb6fb43ff549a
ankush0710 <ankushkurvey053@gmail.com>
2026-07-15 20:04:26 +0530

    add login state based on login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,9 @@
-const getUser = (req, res, next) => {
-    res.render('/login', {
-        pageTitle: "Login",
-        currentPage: "login",
-    })
+exports.getLogin = (req, res, next) => {
+    res.render('auth/login/login', {pageTitle: "Login",})
+}
 
+exports.postLogin = (req, res, next) => {
+    console.log(req.body);
+    req.isLoggedIn = true;
+    res.redirect("/");
 }
