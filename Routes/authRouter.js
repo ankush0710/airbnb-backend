b192cf94f4b6c0c7d72558db0a9629d8231bd677
ankush0710 <ankushkurvey053@gmail.com>
2026-07-15 20:04:35 +0530

    add login page

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -4,6 +4,6 @@ const authRouter = express.Router();
 //import controller for auth
 const authController = require('../controllers/authController');
 
-authRouter.get('/login', authController.getUser);
-
+authRouter.get('/login', authController.getLogin);
+authRouter.post('/login', authController.postLogin)
 module.exports = authRouter;
