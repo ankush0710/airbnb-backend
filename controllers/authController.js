fc9e0a4759dd0e743c7c89d9bfafa848cd1a0228
ankush0710 <ankushkurvey053@gmail.com>
2026-07-16 11:49:46 +0530

    logout button implemented successfully

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,3 +11,8 @@ exports.postLogin = (req, res, next) => {
     // req.isLoggedIn = true;
     res.redirect("/");
 }
+
+exports.postLogout = (req, res, next) => {
+    res.cookie("isLoggedIn", false);
+    res.redirect("/login");
+}
