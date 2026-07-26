074135d508e0758ca073677035dbff20e59ca79a
ankush0710 <ankushkurvey053@gmail.com>
2026-07-26 19:38:28 +0530

    validation for all fields of signup pages is added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const { check } = require("express-validator")
+
 exports.getLogin = (req, res, next) => {
     res.render('auth/login/login', {
         pageTitle: "Login",
@@ -17,10 +19,74 @@ exports.postLogin = (req, res, next) => {
     res.redirect("/");
 }
 
-exports.postSignup = (req, res, next) => {
+exports.postSignup = [
+    // validation for first name 
+    check("fname")
+    .trim()
+    .isLength({min: 2})
+    .withMessage("first name should be atleast 2 characters long")
+    .matches(/^[A-Za-z\s]+$/)
+    .withMessage("first name should contains only letters"),
+
+     // validation for last name 
+    check("lname")
+    .trim()
+    .isLength({min: 2})
+    .withMessage("last name should be atleast 2 characters long")
+    .matches(/^[A-Za-z\s]+$/)
+    .withMessage("last name should contains only letters"),
+
+    //validation for email
+    check("email")
+    .trim()
+    .isEmail()
+    .withMessage("Please enter your valid email")
+    .normalizeEmail(),
+
+    //vaidate for password
+    check("password")
+    .trim()
+    .isLength({min: 8})
+    .withMessage("Password should be atleast 8 character long")
+    .matches(/[A-Z]/)
+    .withMessage("Password should contain atleast one uppercase")
+    .matches(/[a-z]/)
+    .withMessage("Password should contain atleast one lowercase")
+    .matches(/[0-9]/)
+    .withMessage("Password should contain atleast one number")
+    .matches(/[!@#$%^&*.,/?';:]/)
+    .withMessage("Password should contain atleast one special character"),
+
+    //validate for confirm password
+    check("cpassword")
+    .trim()
+    .custom((value, {req}) => {
+        if(value !== req.body.password){
+            throw new Error("Password do not match");
+        }
+        return true
+    }),
+
+    //validate user type
+    check("role")
+    .notEmpty()
+    .withMessage("User type is required")
+    .isIn(['guest', 'admin'])
+    .withMessage("Invalid user type"),
+
+    //validate terms and conditions
+    check("terms")
+    .custom((value) => {
+        if( value !== 'on'){
+            throw new Error("you must accept terms and conditions");
+        }
+        return true
+    }),
+
+(req, res, next) => {
     console.log(req.body);
     res.redirect("/login");
-}
+}] 
 
 exports.postLogout = (req, res, next) => {
     // res.cookie("isLoggedIn", false);
